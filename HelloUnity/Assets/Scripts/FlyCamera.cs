f855549a4013e1765639aaec0a16ecc857c9a1b4
norasteil <nsteil@brynmawr.edu>
2024-09-25 19:01:15 -0400

    Add files via upload

--- HelloUnity/Assets/Scripts/FlyCamera.cs
+++ HelloUnity/Assets/Scripts/FlyCamera.cs
@@ -4,19 +4,41 @@ using UnityEngine;
 
 public class FlyCamera : MonoBehaviour
 {
-    // mouse movement rotates camera up/down and left/right
-    // wasd keys move camera forward/back/left/right
-    // use GetAxis to get the mouse movement each frame
-    
+    public float lookSensitivity = 5.0f;
+    public float moveSpeed = 5.0f;
+    public float fwdFactor = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Cursor.lockState = CursorLockMode.Locked;
+        // Screen.lockCursor = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // wasd mvmt
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        move = transform.TransformDirection(move) * moveSpeed * Time.deltaTime;
+        transform.position += move;
+
+        // mouse scroll
+        Vector3 fwd = Camera.main.transform.forward;
+        Camera.main.transform.position += fwd * Input.mouseScrollDelta.y * fwdFactor;
+
+        // mouse rotation
+        Vector2 mouseDelta = lookSensitivity * new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+        Quaternion rotation = transform.rotation;
+        Quaternion horiz = Quaternion.AngleAxis(mouseDelta.x, Vector3.up);
+        Quaternion vert = Quaternion.AngleAxis(mouseDelta.y, Vector3.right);
+        transform.rotation = vert * rotation * horiz;
+
+        // w: move forward, +z
+        // a: move left, -x
+        // d: move right, +x
+        // s: move backward, -z
+        // moving mouse left/right: rotate around y axis
+        // moving mouse up/down: rotate around x axis
     }
 }
