42bbd44d532de718900bbce92d0d1378d30a5069
Nora Steil <norasteil@wc116-dhcp-72.brynmawr.edu>
2024-09-25 12:12:18 -0400

    A04

---
+++ HelloUnity/Assets/Scripts/FlyCamera.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyCamera : MonoBehaviour
+{
+    // mouse movement rotates camera up/down and left/right
+    // wasd keys move camera forward/back/left/right
+    // use GetAxis to get the mouse movement each frame
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
