a2a0f8883db016f8e5f9b7f90734552637d85fc3
norasteil <nsteil@brynmawr.edu>
2024-11-08 20:30:42 -0500

    A09

---
+++ HelloUnity/Assets/Scripts/Wander.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wander : MonoBehaviour
+{
+    public float radius = 20.0f;
+    public float moveSpeed = 3.0f;
+    private UnityEngine.AI.NavMeshAgent navMesh;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMesh = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        navMesh.speed = moveSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (navMesh.remainingDistance < 0.4f) {
+            Vector3 newDest = newPoint();
+            navMesh.SetDestination(newDest);
+        }
+        // use NavMeshAgent.remainingDistance to check if agent has reached target yet
+        // if close to destination, choose another random point
+    }
+
+    public Vector3 newPoint() 
+    {
+        // get a random point
+        Vector3 sourcePos = (Random.insideUnitSphere * radius) + transform.position;
+        // sourcePos += transform.position;
+        UnityEngine.AI.NavMeshHit hit;
+
+        // find the closest point to the random point that is inside the navMesh and wander radius
+        if (UnityEngine.AI.NavMesh.SamplePosition(sourcePos, out hit, radius, UnityEngine.AI.NavMesh.AllAreas)) {
+            return hit.position;
+        }
+
+        return transform.position;
+    }
+}
